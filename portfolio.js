8df41c0d752f77ee3fc37835a4c2a2bea5b6a5bb
Sagarmore1122 <126661572+Sagarmore1122@users.noreply.github.com>
2024-10-21 00:55:34 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,35 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', function () {
+    const hamIcon = this.querySelector('.hamburger-icon');
+    const crossIcon = this.querySelector('.cross-icon');
+    if (hamIcon.style.display === "none") {
+        hamIcon.style.display = "inline-block"
+        menu.style.display = "none"
+        crossIcon.style.display = "none"
+    }
+    else {
+        crossIcon.style.display = "inline-block"
+        hamIcon.style.display = "none"
+        menu.style.display = "block"
+    }
+});
+
+// timeline
+
+document.addEventListener("DOMContentLoaded", function () {
+    const timelineItems = document.querySelectorAll('.timeline-item');
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('in-view');
+        }
+      });
+    });
+  
+    timelineItems.forEach(item => {
+      observer.observe(item);
+    });
+  });
